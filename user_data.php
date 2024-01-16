84f6f5e3493502a70a4f1830e2a92ba73b6f726a
Nikola Kasabov <91128659+pemerut@users.noreply.github.com>
2024-01-16 17:21:13 +0200

    Update user_data.php

--- user_data.php
+++ user_data.php
@@ -6,13 +6,11 @@
     <title>User's Tags</title>
     <link rel="stylesheet" href="main_style.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
-    <h2 class="centered-title">User's Tags</h2>
-
     <?php
     session_start();
-    require "autorisation.php";
 
     error_reporting(E_ALL);
     ini_set('display_errors', 1);
@@ -21,6 +19,9 @@
         header("Location: login.php");
         exit;
     }
+
+    require "autorisation.php";
+    include 'navbar.php';
 
     $data = $connection->prepare("SELECT tag_id, tag_name, type, currency FROM tags WHERE user_id = ?");
     $data->bind_param("i", $_SESSION["user_id"]);
@@ -68,6 +69,7 @@
         $report_result = $report_data->get_result();
     }
 ?>
+<h2 class="centered-title">User's Tags</h2>
 <div class="form-container">
     <h3>Generate Report</h3>
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
@@ -119,12 +121,13 @@
 <?php endif; ?>
 
 <div class="form-container" style="text-align: center;">
-    <button onclick="window.location.href='main.php'">Back to the Main Page</button>
+    <button onclick="window.location.href='index.php'">Back to the Main Page</button>
 </div>
 
 <?php
 $data->close();
 $connection->close();
 ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
