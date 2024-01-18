ea86c3514817fbe47a956b239b77f35cbe37aafc
Nikola Kasabov <91128659+pemerut@users.noreply.github.com>
2024-01-18 18:28:00 +0200

    Update delete_transaction.php

--- delete_transaction.php
+++ delete_transaction.php
@@ -10,9 +10,9 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true || !isset($_
 $transaction_id = $_GET['id'];
 $user_id = $_SESSION["user_id"];
 
-$stmt = $connection->prepare("DELETE FROM transactions WHERE transaction_id = ? AND user_id = ?");
-$stmt->bind_param("ii", $transaction_id, $user_id);
-$stmt->execute();
+$data = $connection->prepare("DELETE FROM transactions WHERE transaction_id = ? AND user_id = ?");
+$data->bind_param("ii", $transaction_id, $user_id);
+$data->execute();
 
 header("Location: main.php");
 exit;
