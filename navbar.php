2d8453e3403696adf32f234cda8d388db38b9d41
Nikola Kasabov <91128659+pemerut@users.noreply.github.com>
2024-01-18 12:23:49 +0200

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -10,7 +10,7 @@
                         <a class="nav-link btn btn-outline-secondary" href="index.php">Home</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link btn btn-outline-secondary" href="user_data.php">Data</a>
+                        <a class="nav-link btn btn-outline-secondary" href="user_data.php">User's Data</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link btn btn-outline-secondary" href="login.php">Sign out</a>
